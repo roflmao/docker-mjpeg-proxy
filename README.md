eb281bd1600efa8c16541c36ea398c7ceb887a9b
rolf <rolf@supereye.org>
2017-08-15 22:22:29 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # docker-mjpeg-proxy
+
+environment variable MJPEG_URL set:
+
+```
+docker run -it -p 8080:8080 -e MJPEG_URL='http://192.168.66.49:2971/camera/stream'  roflmao/mjpeg-proxy
+```
---
+++ index.js
@@ -0,0 +1,7 @@
+var MjpegProxy = require('mjpeg-proxy').MjpegProxy;
+var express = require('express');
+var app = express();
+var mjpegurl = process.env.MJPEG_URL;
+
+app.get('/index.jpg', new MjpegProxy(mjpegurl).proxyRequest);
+app.listen(8080);
